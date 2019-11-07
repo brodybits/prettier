6fa42400dfe5251d122567756b33f36c7c02c747
Christopher J. Brody <chris@brody.consulting>
2019-11-06 22:40:00 -0500

    update stryker.conf.js to mutate all src trees

--- stryker.conf.js
+++ stryker.conf.js
@@ -4,8 +4,19 @@ module.exports = function(config) {
   config.set({
     mutator: "javascript",
     mutate: [
-      "src/cli/**/*.js"
-      // ...
+      "src/cli/**/*.js",
+      "src/common/**/*.js",
+      "src/config/**/*.js",
+      "src/doc/**/*.js",
+      "src/language-css/**/*.js",
+      "src/language-graphql/**/*.js",
+      "src/language-handlebars/**/*.js",
+      "src/language-html/**/*.js",
+      "src/language-js/**/*.js",
+      "src/language-markdown/**/*.js",
+      "src/language-yaml/**/*.js",
+      "src/main/**/*.js",
+      "src/utils/**/*.js"
     ],
     packageManager: "yarn",
     reporters: ["html", "clear-text", "progress"],
