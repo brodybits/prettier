0c38cf853ed3aaf28c0339074cb6ebbfd4b8ba1c
Christopher J. Brody <chris@brody.consulting>
2019-11-06 22:31:13 -0500

    only mutate JavaScript `src/cli` for now

--- stryker.conf.js
+++ stryker.conf.js
@@ -3,6 +3,10 @@
 module.exports = function(config) {
   config.set({
     mutator: "javascript",
+    mutate: [
+      "src/cli/**/*.js"
+      // ...
+    ],
     packageManager: "yarn",
     reporters: ["html", "clear-text", "progress"],
     testRunner: "jest",
