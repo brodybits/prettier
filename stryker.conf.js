0f95b99ce5405d4e06948e4ad9ee18ade08aaf3d
Christopher J. Brody <chris@brody.consulting>
2019-11-13 19:31:47 +0000

    [TESTING TBD ...] limited src artifacts to mutate for now (at least)
    limited down to src/common at this point

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,6 +1,18 @@
 module.exports = function(config) {
   config.set({
     mutator: "javascript",
+    mutate: [
+      // [FUTURE TBD] this seems to take about 100 hours on a 40-CPU server in the cloud:
+      // "src/**/*.js"
+      // [TBD] mutation testing limited to src/common for now:
+      // "src/cli/*.js"
+      // ... , ...
+      "src/common/*.js"
+      // ... , ...
+      // "src/language-js/*.js"
+      // ... , ...
+      // "src/utils/*.js"
+    ],
     packageManager: "yarn",
     reporters: ["html", "clear-text", "progress"],
     testRunner: "command",
