97d499b578182c7ce19014c9058b87f5f8825fd0
Christopher J. Brody <chris@brody.consulting>
2019-11-13 19:18:44 +0000

    [TBD ???] set test timeout to 15 minutes in stryker.conf.js

--- stryker.conf.js
+++ stryker.conf.js
@@ -4,6 +4,8 @@ module.exports = function(config) {
     packageManager: "yarn",
     reporters: ["html", "clear-text", "progress"],
     testRunner: "command",
+    // TBD ???:
+    timeoutMS: 15*60*1000,
     transpilers: [],
     coverageAnalysis: "off"
   });
