2ac83d1b80ac7eb7703d0d2ca8acc8923c1250a2
Christopher J. Brody <chris.brody@gmail.com>
2020-02-05 12:58:27 -0500

    use proxy modules in internal-plugins to simplify dynamic require imports

--- src/internal-plugins/internal-plugins.js
+++ src/internal-plugins/internal-plugins.js
@@ -12,71 +12,71 @@ module.exports = [
     parsers: {
       // JS - Babel
       get babel() {
-        return eval("require")("../language-js/parser-babylon").parsers.babel;
+        return eval("require")("./parser-babylon").parsers.babel;
       },
       get "babel-flow"() {
-        return eval("require")("../language-js/parser-babylon").parsers[
+        return eval("require")("./parser-babylon").parsers[
           "babel-flow"
         ];
       },
       get "babel-ts"() {
-        return eval("require")("../language-js/parser-babylon").parsers[
+        return eval("require")("./parser-babylon").parsers[
           "babel-ts"
         ];
       },
       get babylon() {
-        return eval("require")("../language-js/parser-babylon").parsers.babel;
+        return eval("require")("./parser-babylon").parsers.babel;
       },
       get json() {
-        return eval("require")("../language-js/parser-babylon").parsers.json;
+        return eval("require")("./parser-babylon").parsers.json;
       },
       get json5() {
-        return eval("require")("../language-js/parser-babylon").parsers.json5;
+        return eval("require")("./parser-babylon").parsers.json5;
       },
       get "json-stringify"() {
-        return eval("require")("../language-js/parser-babylon").parsers[
+        return eval("require")("./parser-babylon").parsers[
           "json-stringify"
         ];
       },
       get __js_expression() {
-        return eval("require")("../language-js/parser-babylon").parsers
+        return eval("require")("./parser-babylon").parsers
           .__js_expression;
       },
       get __vue_expression() {
-        return eval("require")("../language-js/parser-babylon").parsers
+        return eval("require")("./parser-babylon").parsers
           .__vue_expression;
       },
       get __vue_event_binding() {
-        return eval("require")("../language-js/parser-babylon").parsers
+        return eval("require")("./parser-babylon").parsers
           .__vue_event_binding;
       },
       // JS - Flow
       get flow() {
-        return eval("require")("../language-js/parser-flow").parsers.flow;
+        return eval("require")("./parser-flow").parsers.flow;
       },
       // JS - TypeScript
       get typescript() {
-        return eval("require")("../language-js/parser-typescript").parsers
+        return eval("require")("./parser-typescript").parsers
           .typescript;
       },
       // JS - Angular Action
       get __ng_action() {
-        return eval("require")("../language-js/parser-angular").parsers
+        return eval("require")("./parser-angular").parsers
           .__ng_action;
       },
       // JS - Angular Binding
       get __ng_binding() {
-        return eval("require")("../language-js/parser-angular").parsers
+        return eval("require")("./parser-angular").parsers
           .__ng_binding;
       },
       // JS - Angular Interpolation
       get __ng_interpolation() {
-        return eval("require")("../language-js/parser-angular").parsers
+        return eval("require")("./parser-angular").parsers
           .__ng_interpolation;
       },
       // JS - Angular Directive
       get __ng_directive() {
-        return eval("require")("../language-js/parser-angular").parsers
+        return eval("require")("./parser-angular").parsers
           .__ng_directive;
       }
     }
@@ -88,13 +88,13 @@ module.exports = [
     parsers: {
       // TODO: switch these to just `postcss` and use `language` instead.
       get css() {
-        return eval("require")("../language-css/parser-postcss").parsers.css;
+        return eval("require")("./parser-postcss").parsers.css;
       },
       get less() {
-        return eval("require")("../language-css/parser-postcss").parsers.css;
+        return eval("require")("./parser-postcss").parsers.css;
       },
       get scss() {
-        return eval("require")("../language-css/parser-postcss").parsers.css;
+        return eval("require")("./parser-postcss").parsers.css;
       }
     }
   },
@@ -104,7 +104,7 @@ module.exports = [
   {
     parsers: {
       get glimmer() {
-        return eval("require")("../language-handlebars/parser-glimmer").parsers
+        return eval("require")("./parser-glimmer").parsers
           .glimmer;
       }
     }
@@ -115,7 +115,7 @@ module.exports = [
   {
     parsers: {
       get graphql() {
-        return eval("require")("../language-graphql/parser-graphql").parsers
+        return eval("require")("./parser-graphql").parsers
           .graphql;
       }
     }
@@ -126,15 +126,15 @@ module.exports = [
   {
     parsers: {
       get remark() {
-        return eval("require")("../language-markdown/parser-markdown").parsers
+        return eval("require")("./parser-markdown").parsers
           .remark;
       },
       get markdown() {
-        return eval("require")("../language-markdown/parser-markdown").parsers
+        return eval("require")("./parser-markdown").parsers
           .remark;
       },
       get mdx() {
-        return eval("require")("../language-markdown/parser-markdown").parsers
+        return eval("require")("./parser-markdown").parsers
           .mdx;
       }
     }
@@ -145,19 +145,19 @@ module.exports = [
     parsers: {
       // HTML
       get html() {
-        return eval("require")("../language-html/parser-html").parsers.html;
+        return eval("require")("./parser-html").parsers.html;
       },
       // Vue
       get vue() {
-        return eval("require")("../language-html/parser-html").parsers.vue;
+        return eval("require")("./parser-html").parsers.vue;
       },
       // Angular
       get angular() {
-        return eval("require")("../language-html/parser-html").parsers.angular;
+        return eval("require")("./parser-html").parsers.angular;
       },
       // Lightning Web Components
       get lwc() {
-        return eval("require")("../language-html/parser-html").parsers.lwc;
+        return eval("require")("./parser-html").parsers.lwc;
       }
     }
   },
@@ -167,7 +167,7 @@ module.exports = [
   {
     parsers: {
       get yaml() {
-        return eval("require")("../language-yaml/parser-yaml").parsers.yaml;
+        return eval("require")("./parser-yaml").parsers.yaml;
       }
     }
   }
